143570b780698d436f0c3a4ecf52ab1c6344aa31
Nina Mäkinen <sherlocked1223@gmail.com>
2023-08-15 15:55:12 +0300

    Added project base

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
