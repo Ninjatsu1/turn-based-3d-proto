74cc95ccbb0f666d793a62e7904edac1f2c327dd
Nina Mäkinen <sherlocked1223@gmail.com>
2023-08-16 16:32:00 +0300

    Added turn order

---
+++ Assets/ScriptableObjects/Scripts/CharacterStats.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Characters", order = 1)]
+public class CharacterStats : ScriptableObject
+{
+    public string Name = "Name?";
+    public int Attack = 5;
+    public int Health = 20;
+    public int Speed = 10;
+}
--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<CharacterStats> characters = new List<CharacterStats>();
+    [SerializeField] private List<CharacterStats> turnOrder = new List<CharacterStats>();
+
+    private void Start()
     {
-        
+        GetCharacters();
+        SetTurnOrder();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Gets player and enemies
+    private void GetCharacters() 
     {
-        
+        Character[] charactersArray;
+        charactersArray = FindObjectsOfType<Character>();
+        for (int i = 0; i < charactersArray.Length; i++)
+        {
+            characters.Add(charactersArray[i].GetComponent<Character>().CharacterStats);
+        }
+    }
+
+    private void SetTurnOrder()
+    {
+       turnOrder = characters.OrderByDescending(character => character.Speed).ToList();
     }
 }
